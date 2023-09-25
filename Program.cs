81b9f67538b7b6eca8c3dce69a136ba99d50e9a7
viggosjostrom <viggosjostrom1@gmail.com>
2023-09-25 12:55:39 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// variabler
+using System.ComponentModel.Design;
+using System.Runtime.Intrinsics.X86;
+{ 
+var number = 1; 
+
+
+{
+    
+
+
+    if (number % 15 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (number % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else if (number % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else
+    {
+        Console.WriteLine(number);
+    }
+}
+  
+    
+ 
+   
+        
+
+
+
+
+    number++;
+
+
+
+}
+}
